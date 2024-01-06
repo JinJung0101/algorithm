f6a0902f5b718f4c2e9d35d47d1e6a626bed7d8e
JinJung0101 <147036914+JinJung0101@users.noreply.github.com>
2024-01-07 00:05:12 +0900

    코드 수정

--- 테스트연습/11728_배열합치기.py
+++ 테스트연습/11728_배열합치기.py
@@ -5,5 +5,7 @@ a, b = map(int, sys.stdin.readline().split())
 A = list(map(int, sys.stdin.readline().split()))
 B = list(map(int, sys.stdin.readline().split()))
 ans = sorted(A + B)
-for i in ans:
-    print(i, end=' ')
+# for i in ans:
+#     print(i, end=' ')
+# print가 너무 많이 호출되어 속도가 느리다.
+print(' '.join(map(str, ans)))
