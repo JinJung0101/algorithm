8e0aa8d3e171bf10873c469cbbd95064f03683c1
JinJung0101 <147036914+JinJung0101@users.noreply.github.com>
2024-01-11 17:30:58 +0900

    list -> stack 사용해서 풀이 수정

--- 테스트연습/1406_에디터.py
+++ 테스트연습/1406_에디터.py
@@ -1,29 +1,51 @@
 import sys
-strings = list(map(str, sys.stdin.readline().strip()))
+# list를 사용하는 예시, 시간 복잡도 O(n)
+# strings = list(map(str, sys.stdin.readline().strip()))
+# num = int(sys.stdin.readline())
+# cursur = len(strings)
+# for i in range(num):
+#     temp = sys.stdin.readline().strip()
+#     if temp[0] == "L":
+#         if cursur == 0:
+#             continue
+#         else:
+#             cursur -= 1
+#     elif temp[0] == "D":
+#         if cursur == len(strings):
+#             continue
+#         else:
+#             cursur += 1
+#     elif temp[0] == "B":
+#         if cursur == 0:
+#             continue
+#         else:
+#             strings.pop(cursur-1)
+#             cursur -= 1
+#     elif temp[0] == "P":
+#         strings.insert(cursur, temp[2])
+#         cursur += 1
+
+# print(''.join(strings))
+
+left_stack = list(sys.stdin.readline().strip())
+right_stack = []
 num = int(sys.stdin.readline())
-cursur = len(strings)
-for i in range(num):
+for _ in range(num):
     temp = sys.stdin.readline().strip()
     if temp[0] == "L":
-        if cursur == 0:
-            continue
-        else:
-            cursur -= 1
+        if left_stack:
+            right_stack.append(left_stack.pop())
     elif temp[0] == "D":
-        if cursur == len(strings):
-            continue
-        else:
-            cursur += 1
+        if right_stack:
+            left_stack.append(right_stack.pop())
     elif temp[0] == "B":
-        if cursur == 0:
-            continue
-        else:
-            strings.pop(cursur-1)
-            cursur -= 1
+        if left_stack:
+            left_stack.pop()
     elif temp[0] == "P":
-        strings.insert(cursur, temp[2])
-        cursur += 1
+        left_stack.append(temp[2])
 
-print(''.join(strings))
+right_stack.reverse()
+print(''.join(left_stack + right_stack))
+
 
 
