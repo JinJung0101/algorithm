7cc1ce887765045174b17a36df5773d098d6f61c
jinjung0101 <qwe5344641@gmail.com>
2024-03-05 22:21:53 +0900

    풀이 추가

---
+++ 테스트연습/백준/11660.py
@@ -0,0 +1,19 @@
+n, m = map(int, input().split())
+
+arr = []
+
+for i in range(n):
+    a = list(map(int, input().split()))
+    arr.append(a)
+    
+def sum_arr(x1, y1, x2, y2):
+    sum = 0
+    for row in range(x1-1, x2):
+        for col in range(y1-1, y2):
+            sum += arr[row][col]
+    return sum
+            
+for i in range(m):
+    x1, y1, x2, y2 = map(int, input().split())
+    result = sum_arr(x1, y1, x2, y2)
+    print(result)
