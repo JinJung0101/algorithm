99428a08d8f3cc8054ce06cdf9588fb6af3ef627
jinjung0101 <qwe5344641@gmail.com>
2024-03-27 22:56:06 +0900

    풀이 추가

---
+++ 테스트연습/백준/1764.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+d = dict()
+b = []
+ans = 0
+for _ in range(n):
+    d[input().rstrip()] = 1
+for _ in range(m):
+    t = input().rstrip()
+    if t in d:
+        b.append(t)
+        ans += 1
+
+print(ans)
+b.sort()
+for i in b:
