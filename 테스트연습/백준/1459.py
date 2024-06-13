f3569ee5d735e8cea842ebacd0b95ac67c55fda9
jinjung0101 <qwe5344641@gmail.com>
2024-06-13 23:44:32 +0900

    풀이추가

--- 테스트연습/백준/1459.py
+++ 테스트연습/백준/1459.py
@@ -1,13 +1,28 @@
-x, y, forward, cross = map(int, input().split())
+def min_cost_to_reach(X, Y, W, S):
+    x, y = 0, 0  # 출발점 (0, 0)
+    
+    # 모든 직선 이동
+    cost_straight = (X - x + Y - y) * W
+    
+    # 대각선 이동 + 직선 이동
+    diagonal_steps = min(X - x, Y - y)
+    remaining_steps = abs((X - x) - (Y - y))
+    cost_diagonal_and_straight = diagonal_steps * S + remaining_steps * W
+    
+    # 대각선 이동만 사용하는 경우
+    # (대각선 이동이 직선 이동 두 번보다 저렴한 경우)
+    cost_diagonal_only = float('inf')
+    if S < 2 * W:
+        if remaining_steps % 2 == 0:
+            cost_diagonal_only = max(X - x, Y - y) * S
+        else:
+            cost_diagonal_only = (max(X - x, Y - y) - 1) * S + W
+    
+    # 최소 비용 계산
+    min_cost = min(cost_straight, cost_diagonal_and_straight, cost_diagonal_only)
+    
+    return min_cost
 
-m = min(x, y)
-M = max(x, y)
-if cross < forward:
-    if (M-m) % 2 == 0:
-        print(M * cross)
-    else:
-        print((M-1) * cross + forward)
-elif cross < forward * 2:
-    print(m * cross + (M-m) * forward)
-else:
-    print((m+M) * forward)
+# 입력 받기
+X, Y, W, S = map(int, input().split())
+print(min_cost_to_reach(X, Y, W, S))
