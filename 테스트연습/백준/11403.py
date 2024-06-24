4cc53ae489b76836df49ee977e786e1da53b72b4
jinjung0101 <qwe5344641@gmail.com>
2024-06-24 23:13:24 +0900

    풀이추가

---
+++ 테스트연습/백준/11403.py
@@ -0,0 +1,14 @@
+# 입력 받기
+N = int(input())
+graph = [list(map(int, input().split())) for _ in range(N)]
+
+# 플로이드-워셜 알고리즘 수행
+for k in range(N):
+    for i in range(N):
+        for j in range(N):
+            if graph[i][k] and graph[k][j]:
+                graph[i][j] = 1
+
+# 결과 출력
+for row in graph:
+    print(" ".join(map(str, row)))
