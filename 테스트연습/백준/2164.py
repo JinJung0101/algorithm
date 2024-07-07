92a6b0dc3ef2392ca7efb29af61264b1498323d6
jinjung0101 <qwe5344641@gmail.com>
2024-07-07 23:59:08 +0900

    풀이추가

--- 테스트연습/백준/2164.py
+++ 테스트연습/백준/2164.py
@@ -1,11 +1,17 @@
 from collections import deque
 
-N = int(input())
-deque = deque([i for i in range(1, N+1)])
-
-while(len(deque) >1):
-    deque.popleft()
-    move_num = deque.popleft()
-    deque.append(move_num)
+def main():
+    import sys
+    input = sys.stdin.read
+    N = int(input().strip())
     
-print(deque[0])
+    queue = deque(range(1, N + 1))
+    
+    while len(queue) > 1:
+        queue.popleft()  # 제일 위의 카드를 버립니다.
+        queue.append(queue.popleft())  # 그 다음 카드를 빼서 제일 아래에 놓습니다.
+    
+    print(queue[0])  # 마지막 남은 카드를 출력합니다.
+
+if __name__ == "__main__":
+    main()
