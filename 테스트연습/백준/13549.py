0e64ace77a82994b649d19dfafdc53682a581e60
jinjung0101 <qwe5344641@gmail.com>
2024-08-19 23:21:20 +0900

    풀이추가

---
+++ 테스트연습/백준/13549.py
@@ -0,0 +1,28 @@
+from collections import deque
+
+def bfs(n, k):
+    if n >= k:
+        return n - k
+    
+    deque_ = deque([n])
+    visited = [float('inf')] * 100001
+    visited[n] = 0
+
+    while deque_:
+        current = deque_.popleft()
+        time = visited[current]
+        
+        if current * 2 <= 100000 and visited[current * 2] > time:
+            visited[current * 2] = time
+            deque_.appendleft(current * 2)
+        if current + 1 <= 100000 and visited[current + 1] > time + 1:
+            visited[current + 1] = time + 1
+            deque_.append(current + 1)
+        if current - 1 >= 0 and visited[current - 1] > time + 1:
+            visited[current - 1] = time + 1
+            deque_.append(current - 1)
+
+    return visited[k]
+
+n, k = map(int, input().split())
+print(bfs(n, k))
