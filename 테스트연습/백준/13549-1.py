319e6cf6a57f1a146bef0a84afef2dc8c0a612bc
jinjung0101 <qwe5344641@gmail.com>
2024-08-20 23:59:50 +0900

    풀이추가

--- 테스트연습/백준/13549-1.py
+++ 테스트연습/백준/13549-1.py
@@ -1,1 +1,26 @@
 #dp사용
+
+def min_time(n, k):
+    if n >= k:
+        return n - k
+    
+    dp = [float('inf')] * 100001
+    dp[n] = 0
+    queue = [n]
+    
+    while queue:
+        current = queue.pop(0)
+        
+        if current * 2 <= 100000 and dp[current * 2] > dp[current]:
+            dp[current * 2] = dp[current]
+            queue.append(current * 2)
+        
+        for next_pos in [current - 1, current + 1]:
+            if 0 <= next_pos <= 100000 and dp[next_pos] > dp[current] + 1:
+                dp[next_pos] = dp[current] + 1
+                queue.append(next_pos)
+                
+    return dp[k]
+
+n, k = map(int, input().split())
+print(min_time(n, k))
