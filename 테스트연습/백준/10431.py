ed314534ba92abd4b552d82809b829cc007c88a9
jinjung0101 <qwe5344641@gmail.com>
2024-08-29 23:16:53 +0900

    풀이추가

--- 테스트연습/백준/10431.py
+++ 테스트연습/백준/10431.py
@@ -28,3 +28,47 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+
+
+#다른풀이 ------------------------------------------
+def update(index, value, tree, max_val):
+    while index <= max_val:
+        tree[index] += value
+        index += index & (-index)
+
+def query(index, tree):
+    sum = 0
+    while index > 0:
+        sum += tree[index]
+        index -= index & (-index)
+    return sum
+
+def count_inversions(array):
+    max_val = max(array)
+    tree = [0] * (max_val + 1)
+    inversions = 0
+    for i in range(len(array)):
+        inversions += query(max_val, tree) - query(array[i], tree)
+        update(array[i], 1, tree, max_val)
+    return inversions
+
+def main():
+    import sys
+    input = sys.stdin.read
+    data = input().splitlines()
+    p = int(data[0])  # 테스트 케이스의 수
+    results = []
+
+    for i in range(1, p + 1):
+        line = data[i].split()
+        index = line[0]
+        array = list(map(int, line[1:]))
+        inversions = count_inversions(array)
+        results.append(f"{index} {inversions}")
+
+    for result in results:
+        print(result)
+
+if __name__ == "__main__":
+    main()
