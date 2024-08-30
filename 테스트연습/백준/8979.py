c837ff392c5db1fdf5e816d278828e18b77f6822
jinjung0101 <qwe5344641@gmail.com>
2024-08-30 23:05:25 +0900

    풀이추가

--- 테스트연습/백준/8979.py
+++ 테스트연습/백준/8979.py
@@ -22,3 +22,40 @@ for i in range(n):
     if score[i][3] == k:
         print(idx)
         break
+
+
+
+# 다른 풀이 -------------------------------------------
+
+input = sys.stdin.read
+
+def main():
+    data = input().split()
+    n, k = int(data[0]), int(data[1])
+    scores = []
+    nation_index = {}
+
+    index = 2
+    for _ in range(n):
+        nation = int(data[index])
+        gold = int(data[index+1])
+        silver = int(data[index+2])
+        bronze = int(data[index+3])
+        scores.append((gold, silver, bronze, nation))
+        nation_index[nation] = (gold, silver, bronze)
+        index += 4
+
+    # 메달 수에 따라 정렬
+    scores.sort(reverse=True, key=lambda x: (x[0], x[1], x[2]))
+
+    # 순위 찾기
+    rank = 1
+    for i, score in enumerate(scores):
+        if i > 0 and score[:3] != scores[i-1][:3]:
+            rank = i + 1
+        if score[3] == k:
+            print(rank)
+            break
+
+if __name__ == "__main__":
+    main()
