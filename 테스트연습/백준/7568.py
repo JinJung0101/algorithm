7e34b67537be08f13db49a441ef04361ab429a3e
jinjung0101 <qwe5344641@gmail.com>
2024-08-31 23:43:03 +0900

    풀이추가

--- 테스트연습/백준/7568.py
+++ 테스트연습/백준/7568.py
@@ -12,3 +12,37 @@ for i in range(n):
         if arr[i][0] < arr[j][0] and arr[i][1] < arr[j][1]:
             temp += 1
     print(temp, end=' ')
+
+
+
+
+
+# 다른 풀이 -----------------------------------------------
+
+
+input = sys.stdin.readline
+
+def calculate_rank(people):
+    n = len(people)
+    ranks = [1] * n  # 모든 사람의 초기 등수는 1로 시작
+
+    for i in range(n):
+        for j in range(n):
+            if people[i]['kg'] < people[j]['kg'] and people[i]['cm'] < people[j]['cm']:
+                ranks[i] += 1
+
+    return ranks
+
+def main():
+    n = int(input().strip())
+    people = []
+
+    for _ in range(n):
+        kg, cm = map(int, input().split())
+        people.append({'kg': kg, 'cm': cm})
+
+    ranks = calculate_rank(people)
+    print(' '.join(map(str, ranks)))
+
+if __name__ == "__main__":
+    main()
